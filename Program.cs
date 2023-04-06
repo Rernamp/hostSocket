863cbcb2efaa969a7307a08c052053fe197001e7
rernamp <renamp100@gmail.com>
2023-04-06 21:25:53 +0300

    add receive from device

--- Program.cs
+++ Program.cs
@@ -10,16 +10,28 @@ namespace Application {
             string ip = "192.168.3.207";
             socket.Connect(IPAddress.Parse(ip), 80);
 
+            byte[] data = { 0xFE };
+
+            var res = socket.Send(data, SocketFlags.None);
+
+            Console.WriteLine(res);
+
             byte[] buffer = new byte[1024];
             int numberRepeat = 10000;
             while (numberRepeat != 0) {
                 numberRepeat--;
-                if (socket.Receive(buffer) != 0) {
-                    string value = System.Text.Encoding.UTF8.GetString(buffer);
+                var size = socket.Receive(buffer);
+                if (size != 0) {
+                   string value = System.Text.Encoding.UTF8.GetString(buffer);
+ 
+                    for (var i = 0; i < size; i++) 
+                    {
+                        Console.Write($"{buffer[i]} ");                        
+                    }
+                    Console.WriteLine("");
 
-                    Console.WriteLine(value);
 
-                    
+
                 }
                 Thread.Sleep(1);
             }
